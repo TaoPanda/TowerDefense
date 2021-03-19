fefc6486b7cfd6d2fd9383c08572911fa4ce6d86
TaoPanda <erik2708@campusvajle.dk>
2021-03-19 10:57:14 +0100

    Models lavet for Enemy og Tower

---
+++ TowerDefense/Model/EnemyModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TowerDefense.Model
+{
+    class EnemyModel
+    {
+        public string name;
+        public int hp;
+        public int ms;
+        public int xp;
+        public int position;
+        public int cost;
+        public string color;
+
+        public EnemyModel(string name, int hp, int ms, int xp, int position, int cost, string color)
+        {
+            this.name = name;
+            this.hp = hp;
+            this.ms = ms;
+            this.xp = xp;
+            this.position = position;
+            this.cost = cost;
+            this.color = color;
+        }
+    }
+}
---
+++ TowerDefense/Model/TowerModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TowerDefense.Model
+{
+    class TowerModel
+    {
+        public string name;
+        public int range;
+        public int dmg;
+        public int fr;
+        public int cost;
+        public int lvl;
+        public int xp;
+        public int size;
+        public string color;
+
+        public TowerModel(string name, int range, int dmg, int fr, int cost, int lvl, int xp, int size, string color)
+        {
+            this.name = name;
+            this.range = range;
+            this.dmg = dmg;
+            this.fr = fr;
+            this.cost = cost;
+            this.lvl = lvl;
+            this.xp = xp;
+            this.size = size;
+            this.color = color;
+        }
+    }
+}
