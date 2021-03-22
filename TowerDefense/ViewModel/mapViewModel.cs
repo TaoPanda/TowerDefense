5f8ae2a409645126199a2ca0a4487885cebacfe1
Christian Villadsen <christianbjerrumvilladsen@hotmail.com>
2021-03-22 13:51:46 +0100

    Added MapViewModel

---
+++ TowerDefense/ViewModel/mapViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace TowerDefense.ViewModel
+{
+    class mapViewModel
+    {
+        private void GenerateRoute()
+        {
+
+
+        }
+
+       
+    }
+}
