4a37d64489bf330b66866959300e811e31b263cd
TaoPanda <62134703+TaoPanda@users.noreply.github.com>
2021-03-22 14:37:35 +0100

    made GetCenterOfCell method

--- TowerDefense/ViewModel/mapViewModel.cs
+++ TowerDefense/ViewModel/mapViewModel.cs
@@ -13,6 +13,24 @@ namespace TowerDefense.ViewModel
 
         }
 
-       
+       private Array GetCenterOfCell(float cordinat,int cellSize)
+        {
+            int x = Convert.ToInt32(Math.Floor(cordinat));
+            float s = cordinat - x;
+            int y = 0;
+            while(y != s)
+            {
+                try
+                {
+                    y = Convert.ToInt32(s);
+                }
+                catch
+                {
+                    s = s * 10;
+                }
+            }
+            Array cords = new Array[x*cellSize,y*cellSize];
+            return cords;
+        }
     }
 }
