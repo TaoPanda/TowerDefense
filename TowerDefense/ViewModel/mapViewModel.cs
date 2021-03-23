e600df9a6027be022ba306df0d25ab8caa516790
TaoPanda <62134703+TaoPanda@users.noreply.github.com>
2021-03-23 11:38:30 +0100

    made an enemy movement method

--- TowerDefense/ViewModel/mapViewModel.cs
+++ TowerDefense/ViewModel/mapViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using TowerDefense.Model;
 
 namespace TowerDefense.ViewModel
 {
@@ -32,10 +33,10 @@ namespace TowerDefense.ViewModel
 
         }
 
-       private Array GetCenterOfCell(float cordinat,int cellSize)
+       public Array GetCenterOfCell(double cordinat,int cellSize)
         {
             int x = Convert.ToInt32(Math.Floor(cordinat));
-            float s = cordinat - x;
+            double s = cordinat - x;
             int y = 0;
             float half = cellSize / 2;
             int roundhalf = Convert.ToInt32(Math.Round(half));
@@ -52,6 +53,29 @@ namespace TowerDefense.ViewModel
             }
             Array cords = new Array[x * cellSize + roundhalf, y * cellSize + roundhalf];
             return cords;
+        }
+
+        private void MoveEnemyInList(ObservableCollection<EnemyModel> enemies, ObservableCollection<double> route, int cellSize)
+        {
+            List<int> removeIndex = new List<int>();
+            int remove = 0;
+            foreach(EnemyModel enemy in enemies)
+            {
+                enemy.NextPosition();
+                if(enemy.position != route.Count)
+                {
+                    enemy.cordinates = GetCenterOfCell(route[enemy.position], cellSize);
+                }
+                else
+                {
+                    removeIndex.Add(remove);
+                }
+                remove++;
+            }
+            foreach(int index in removeIndex)
+            {
+                enemies.RemoveAt(index);
+            }
         }
     }
 }
