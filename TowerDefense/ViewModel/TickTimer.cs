120e6d904cc1892602a3155f87fde0e5300f9a3c
Jimmy-B-Poulsen <62134665+Jimmy-B-Poulsen@users.noreply.github.com>
2021-03-23 10:47:28 +0100

    Tick Timer that ticks 1/10th a second

---
+++ TowerDefense/ViewModel/TickTimer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Timers;
+using System.Collections.ObjectModel;
+
+public class TickTimer
+{
+	public TickTimer()
+	{
+		Timer timer = new Timer();
+		timer.Interval = 100; //ticks every 1/10th of a second.
+		timer.Tick += new EventHandler(GameMethods());	
+			
+		public void CheckTimerActive(ObservableCollection<enemy> listOfEnemies)
+        {
+            if (listOfEnemies.Count == 0)
+            {
+				timer.Stop;
+            }
+		}
+
+		public void GameMethods(ObservableCollection<enemy> listOfEnemies)
+        {
+			CheckTimerActive(listOfEnemies);
+        }
+	}
+}
