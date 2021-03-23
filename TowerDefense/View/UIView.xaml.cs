511a38853cb209a2a4b55337636c9a4680aa0dbc
TaoPanda <62134703+TaoPanda@users.noreply.github.com>
2021-03-23 11:05:44 +0100

    added a UIView file

---
+++ TowerDefense/View/UIView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TowerDefense.View
+{
+    /// <summary>
+    /// Interaction logic for UIView.xaml
+    /// </summary>
+    public partial class UIView : Window
+    {
+        public UIView()
+        {
+            InitializeComponent();
+        }
+    }
+}
